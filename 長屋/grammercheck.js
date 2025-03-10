fa885b9f289eb1c07fb3a0c48d234bae8e84ef1d
kyougi-kai <pb.kyougikai@gmail.com>
2025-03-10 11:33:45 +0900

    up

--- 長屋/grammercheck.js
+++ 長屋/grammercheck.js
@@ -1,1 +1,6 @@
-npm install compromise;
+const nlp = require('compromise');
+
+let doc = nlp("She plays soccer.");
+console.log(doc.verbs().out('array'));
+console.log(doc.nouns().out('array'));
+console.log(doc.people().out('array'));
