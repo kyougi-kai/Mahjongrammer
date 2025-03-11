d23789020e3df8586a3c992c3f76aa6278ab0d9a
kyougi-kai <zsansirokuma@gmail.com>
2025-03-11 11:39:54 +0900

    up
    がんばった！

--- public/js/play.js
+++ public/js/play.js
@@ -1,3 +1,12 @@
-function ei(){
-    alert('yoyoyo');
+let socket;
+const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+window.onload = () => {
+    const parentName = document.getElementById('parentnameText').textContent;
+    if(parentName != document.getElementById('usernameText').textContent){
+        socket = new WebSocket(`${protocol}://${window.location.host}/room`);
+
+        socket.onopen = () => {
+            socket.send(JSON.stringify({entryRoom:parentName}));
+        }
+    }
 }
