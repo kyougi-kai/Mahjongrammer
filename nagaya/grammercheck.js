5f9935842f39cf4097d332f20df1a3168e3f0f55
kyougi-kai <pb.kyougikai@gmail.com>
2025-03-24 16:15:50 +0900

    any

--- nagaya/grammercheck.js
+++ nagaya/grammercheck.js
@@ -1,15 +1,57 @@
-
-    tango = {
-        apple : {
-            mean: ["りんご"]
-        },
-        wood : {
-            mean: ["木製"]
-        }
-    }
-    let objectmean = "apple";
-    let tangoGo = tango.objectmean;
-    function tangoviews(){
-        console.log(12345689);
-        console.log(tangoGo);
-    }
+tango = {
+  apple: {
+    hinsi: ["名詞"],
+    tags: ["可算名詞"]
+  },
+  desk: {
+    hinsi: ["名詞"],
+    tags: ["可算名詞"]
+  },
+  cat: {
+    hinsi: ["名詞"],
+    tags: ["可算名詞"]
+  },
+  book: {
+    hinsi: ["名詞"],
+    tags: ["可算名詞"]
+  },
+  water: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  happiness: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  money: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  information: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  Tokyo: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  Einstein: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  Amazon: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  iPhone: {
+    hinsi: ["名詞"],
+    tags: ["不可算名詞"]
+  },
+  
+}
+let objectmean = "apple";
+let tangoGo = tango.objectmean;
+function tangoviews(){
+    console.log(12345689);
+    console.log(tangoGo);
+}
