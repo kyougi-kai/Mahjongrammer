a1fedc884634398172a45bcaa559d59aa7cd89bb
kyougi-kai <zsansirokuma@gmail.com>
2025-03-27 11:27:29 +0900

    割合修正

--- public/js/until/dataManager.js
+++ public/js/until/dataManager.js
@@ -1,6 +1,18 @@
 export class DM{
     constructor(){
         this._wariai = [1,1,1,1,1,1,1,1,1,1];
+
+        let sum = 0;
+        this._wariai.forEach((value) => {
+            sum += value;
+        })
+
+        let temporaryWariai = this._wariai.concat();
+        this._wariai = [];
+        temporaryWariai.forEach((value) => {
+            this._wariai.push(value / sum);
+        });
+
         this._partOfSpeech = ["meisi", "dousi", "keiyousi", "hukusi", "daimeisi", "zentisi", "setuzokusi", "kantansi", "kansi", "jodousi"];
         this._tango = {
             meisi : ["dog","school","book","car","apple","city","teacher","computer","river","mountain","idea","happiness","music","game","house","door","flower","table","pencil","phone","actor","actress","advice","air","airplane","airport","animal","answer","area","arm","artist","athlete","author","baby","bag","ball","banana","band","bank","bar","bath","bed","beef","beginner","bike","bird","birthday","blanket","blood","board","boat","body","book","border","bottle","box","boy","bread","brother","building","bus","cake","camera","capital","car","card","carpet","cat","center","chance","change","chicken","child","class","classroom","clothes","cloud","coffee","color","company","computer","cousin","cow","cup","desk","dog","door","down","dream","drink","drum","ear","earth","egg","engine","event","example","eye","face","family","fan","farmer","father","feet","field","file","fish","flower","foot","friend","game","garden","gate","girl","glove","goal","grandfather","grandmother","group","guide","gym","hair","hand","hat","head","heart","home","hospital","house","idea","invention","island","jacket","job","juice","key","king","kitchen","lady","lamp","land","language","laundry","law","leader","leg","library","life","light","line","lip","lock","lunch","machine","man","market","match","meal","medicine","member","menu","message","minute","mirror","money","month","movie","museum","music","name","nephew","news","night","nose","number","object","office","opinion","orange","owner","paint","paper","parent","park","pen","people","photo","picture","place","plane","plate","pool","post","prize","queen","question","radio","rain","restaurant","room","sail","salt","school","season","secretary","seed","shelf","shirt","shoe","singer","sister","snow","soccer","song","south","space","speaker","spot","star","station","stomach","student","subway","sugar","summer","sunglasses","table","tail","team","television","test","thing","thought","ticket","time","tooth","town","toy","traffic","train","transport","tree","umbrella","uncle","uniform","village","voice","water","week","wife","window","winter","woman","world","year","yellow","yoga","zoo"],
