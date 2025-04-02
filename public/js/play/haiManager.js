df7ed7f0ae0af1ed0ff546056a18c3d267924a7e
kyougi-kai <zsansirokuma@gmail.com>
2025-04-02 14:58:46 +0900

    なんかできた

--- public/js/play/haiManager.js
+++ public/js/play/haiManager.js
@@ -1,186 +1,235 @@
-export class HM{
-    constructor(throwEvent){
-        this._idCounter = 0;
-        this._draggedElement = null;
-        this._originalParent = null;
-        this._movedAnotherParent = false;
-        this._selectionDiv = document.getElementById('selectionDiv');
-        this._divisions = document.getElementsByClassName('division-div');
-        this._wordUpRight = document.getElementById('wordUpRight');
-        this._wordDown = document.getElementById('wordDown');
+export class HM {
+  constructor(throwEvent) {
+    this._draggedElement = null;
+    this._originalParent = null;
+    this._movedAnotherParent = false;
+    this._sentencePatterns = Array.from(
+      document.getElementById("sentencePattern").children
+    );
+    this._divisions = document.getElementsByClassName("division-div");
+    this._wordDown = document.getElementById("wordDown");
 
-        Array.from(this._selectionDiv.children).forEach((value) => {
-            value.addEventListener('click', (event) => {
-                console.log('yobareta');
-                const targetText = (event.target.textContent);
-                this.showDivision(targetText.split(''));
-                this.changeCondition(this._selectionDiv);
-                this.changeCondition(this._wordUpRight, true);
-            });
-        });
+    this._isMyTurn = false;
+    this._isBark = false;
 
+    this._sentencePatterns.forEach((value) => {
+      const temporaryDiv = document.createElement("div");
+      temporaryDiv.classList.add("division-div");
+      temporaryDiv.classList.add(`division-${value.textContent.toLowerCase()}`);
+      this.attachDraggable(value, temporaryDiv);
+    });
+
+    Array.from(document.getElementsByClassName("hai-table")).forEach(
+      (value) => {
+        this.attachDraggabled(value);
+      }
+    );
+
+    //捨てる
+    document.addEventListener("drop", (event) => {
+      console.log(this._draggedElement);
+      event.preventDefault();
+      if (this._draggedElement.classList.contains("border-div") && (this._isMyTurn || this._isBark)) {
+        throwEvent(this._draggedElement.outerHTML, this._isBark);
+        this._draggedElement.remove();
         this._isMyTurn = false;
         this._isBark = false;
-
-        //リセットボタン
-        this._wordUpRight.children[0].addEventListener('click', (event) => {
-            Array.from(this._divisions).forEach(element => {
-                if(element.style.opacity = '1'){
-                    while(Array.from(element.children).length != 0){
-                        this._wordDown.appendChild(element.children[0]);
-                    }
-                    this.changeCondition(element);
-                }
-            });
-
-            this.changeCondition(this._selectionDiv);
-            this.changeCondition(this._wordUpRight, true);
+      } 
+      else {
+        let words = this._draggedElement.getElementsByClassName('border-div');
+        Array.from(words).forEach((value) => {
+          this._wordDown.appendChild(value);
         });
-
-        const draggBoxList = Array.from(this._divisions).concat(this._wordDown);
-        draggBoxList.forEach((parent, index) => {
-            parent.addEventListener('dragover', event => {
-                event.preventDefault();//ドロップの可能
-                if (!this._draggedElement) return;
-
-                const childrenCount = Array.from(parent.children).length;
-                if(childrenCount == 0 || event.clientX > parent.children[childrenCount - 1]?.getBoundingClientRect().right){
-                    parent.appendChild(this._draggedElement);
-                }
-
-                    Array.from(parent.children).forEach((hai) => {
-                        if (hai !== this._draggedElement) {//自分はチェックしない
-                            let rect = hai.getBoundingClientRect();
-                            if (event.clientX > rect.left && event.clientX < rect.right && event.clientY > rect.top && event.clientY < rect.bottom) {
-                                 /*
-                                event.clientがマウスのX,Y
-                                重なっているかの判定
-                                */
-                                const idx = Array.from(parent.children).indexOf(hai);
-                                console.log(idx);
-                                console.log(Array.from(parent.children).indexOf(this._draggedElement));
-                                if(!this._movedAnotherParent && this._originalParent != hai.parentNode){
-                                    hai.parentNode.insertBefore(this._draggedElement, hai);
-                                }
-                                else if(idx > Array.from(parent.children).indexOf(this._draggedElement)){
-                                    hai.parentNode.insertBefore(this._draggedElement, hai.nextSibling);
-                                }
-                                else{
-                                    hai.parentNode.insertBefore(this._draggedElement, hai);
-                                }
-
-                                if(this._originalParent != hai.parentNode)this._movedAnotherParent = true;
-                            }
-                        }
-                    })
-            });
-            parent.addEventListener('drop', event => {
-                event.preventDefault();
-                if(this._draggedElement){
-                    this._draggedElement.style.opacity = '1'
-                    this._draggedElement = null;
-                }
-            });
+        this._draggedElement.remove();
+        
+        const sentenceDivs = document.getElementsByClassName('sentence-div');
+        Array.from(sentenceDivs).forEach((value) => {
+          console.log(value);
+          if(!value.children[0])value.remove();
         });
+      }
+    });
 
-        document.addEventListener('dragover', event => {
-            event.preventDefault();
-        });
+    document.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+  }
 
-        //捨てる
-        document.addEventListener('drop', event => {
-            event.preventDefault();
-            if (this._draggedElement && (this._isMyTurn || this._isBark)){
-                throwEvent(this._draggedElement.outerHTML, this._isBark);
-                this._draggedElement.remove();
-                this._isMyTurn = false;
-                this._isBark = false;
+  get divisions() {
+    return this._divisions;
+  }
+
+  /**
+   * @param {string[]} targetPOS
+   */
+  showDivision(targetPOS) {
+    let numOfO = 0;
+    this.changeCondition(this._divisions[0]);
+    this.changeCondition(this._divisions[1]);
+    targetPOS.forEach((value) => {
+      switch (value) {
+        case "C":
+          this.changeCondition(this._divisions[2]);
+          break;
+        case "O":
+          this.changeCondition(this._divisions[3 + numOfO]);
+          numOfO++;
+          break;
+      }
+    });
+  }
+
+  changeCondition(element, dontChangePosition = false) {
+    if (element.style.opacity == "0") {
+      element.style.opacity = "1";
+      if (!dontChangePosition) element.style.position = "relative";
+      element.style.pointerEvents = "all";
+      element.style.zIndex = "0";
+    } else {
+      element.style.opacity = "0";
+      if (!dontChangePosition) element.style.position = "absolute";
+      element.style.pointerEvents = "none";
+      element.style.zIndex = "-1";
+    }
+  }
+
+  set isMyTurn(value) {
+    this._isMyTurn = value;
+  }
+
+  set isBark(value) {
+    this._isBark = value;
+  }
+
+  showHai(word, partOfSpeech) {
+    const borderDiv = document.createElement("div");
+    const wordP = document.createElement("p");
+    borderDiv.classList.add("border-div");
+
+    wordP.textContent = word;
+
+    // ドラッグアンドドロップを有効にする
+    this.attachDraggable(borderDiv);
+
+    //画像
+    borderDiv.style.backgroundImage = `url(/img/partOfSpeech/${partOfSpeech}.png)`;
+
+    borderDiv.appendChild(wordP);
+    document.getElementById("wordDown").appendChild(borderDiv);
+
+    return borderDiv;
+  }
+
+  /**
+   *
+   * @param {HTMLElement} targetElement -ドラッグを可能にする要素-
+   * @param {HTMLElement} dragElement -ドラッグしているときマウスポインターについてくる要素(省略可)-
+   */
+  attachDraggable(targetElement, dragElement = null) {
+    targetElement.setAttribute("draggable", "true");
+
+    targetElement.addEventListener("dragstart", (event) => {
+      event.stopPropagation(); //親要素に伝播しないようにする
+      this._movedAnotherParent = false;
+
+      if (dragElement == null) {
+        this._draggedElement = event.target;
+      } else {
+        const dragElementCopy = dragElement.cloneNode(true);
+        this._draggedElement = dragElementCopy;
+        this.attachDraggable(this._draggedElement);
+        this.attachDraggabled(this._draggedElement, 'division-div');
+      }
+      setTimeout(() => (this._draggedElement.style.opacity = "0.25"), 1);
+      this._originalParent = this._draggedElement.parentNode; // ドラッグ開始時の親要素を保存
+    });
+
+    targetElement.addEventListener("dragend", (event) => {
+      event.stopPropagation(); //親要素に伝播しないようにする
+      if (!this._draggedElement) return;
+      this._draggedElement.style.opacity = "1"; // ドラッグ終了時に元に戻す
+      this._draggedElement = null;
+    });
+  }
+
+  attachDraggabled(targetElement, excludeClass) {
+    targetElement.addEventListener("dragover", (event) => {
+      event.stopPropagation(); //親要素に伝播しないようにする
+      event.preventDefault(); //ドロップの可能
+      if (!this._draggedElement ||this._draggedElement?.classList.contains(excludeClass))return;
+
+      const childrenCount = Array.from(targetElement.children).length;
+      if (childrenCount == 0 || event.clientX > targetElement.children[childrenCount - 1]?.getBoundingClientRect().right) {
+        targetElement.appendChild(this._draggedElement);
+      }
+
+      Array.from(targetElement.children).forEach((hai) => {
+        if (hai !== this._draggedElement) {
+          //自分はチェックしない
+          let rect = hai.getBoundingClientRect();
+          if (
+            event.clientX > rect.left &&
+            event.clientX < rect.right &&
+            event.clientY > rect.top &&
+            event.clientY < rect.bottom
+          ) {
+            /*
+								event.clientがマウスのX,Y
+								重なっているかの判定
+								*/
+            const idx = Array.from(targetElement.children).indexOf(hai);
+            console.log(idx);
+            console.log(
+              Array.from(targetElement.children).indexOf(this._draggedElement)
+            );
+            if (
+              !this._movedAnotherParent &&
+              this._originalParent != hai.parentNode
+            ) {
+              hai.parentNode.insertBefore(this._draggedElement, hai);
+            } else if (
+              idx >
+              Array.from(targetElement.children).indexOf(this._draggedElement)
+            ) {
+              hai.parentNode.insertBefore(
+                this._draggedElement,
+                hai.nextSibling
+              );
+            } else {
+              hai.parentNode.insertBefore(this._draggedElement, hai);
             }
-        });
-    }
 
-    get divisions(){
-        return this._divisions;
-    }
-
-    /**
-     * @param {string[]} targetPOS 
-     */
-    showDivision(targetPOS){
-        let numOfO = 0;
-        this.changeCondition(this._divisions[0]);
-        this.changeCondition(this._divisions[1]);
-        targetPOS.forEach(value => {
-            switch(value){
-                case 'C':
-                    this.changeCondition(this._divisions[2]);
-                    break;
-                case 'O':
-                    this.changeCondition(this._divisions[3 + numOfO]);
-                    numOfO++;
-                    break;
-            }
-        });
-    }
-
-    changeCondition(element, dontChangePosition = false){
-        if(element.style.opacity == '0'){
-            element.style.opacity = '1';
-            if(!dontChangePosition)element.style.position = 'relative';
-            element.style.pointerEvents = 'all';
-            element.style.zIndex = '0';
+            if (this._originalParent != hai.parentNode)
+              this._movedAnotherParent = true;
+          }
         }
-        else{
-            element.style.opacity = '0';
-            if(!dontChangePosition)element.style.position = 'absolute';
-            element.style.pointerEvents = 'none';
-            element.style.zIndex = '-1';
+      });
+    });
+    targetElement.addEventListener("drop", (event) => {
+      event.stopPropagation(); //親要素に伝播しないようにする
+      event.preventDefault();
+      if (this._draggedElement) {
+        this._draggedElement.style.opacity = "1";
+
+        //くくりを作る
+        if (
+          this._draggedElement.classList.contains("division-div") &&
+          this._draggedElement.parentElement.classList.contains("hai-table")
+        ) {
+          const temporarySentence = document.createElement("div");
+          temporarySentence.classList.add("sentence-div");
+          this._draggedElement.parentElement.appendChild(temporarySentence);
+          this.attachDraggable(temporarySentence);
+          this.attachDraggabled(temporarySentence, "sentence-div");
+          temporarySentence.appendChild(this._draggedElement);
         }
-    }
 
-    set isMyTurn(value){
-        this._isMyTurn = value;
-    }
+        this._draggedElement = null;
+      }
+    });
 
-    set isBark(value){
-        this._isBark = value;
-    }
-
-    showHai(word, partOfSpeech){
-        const borderDiv = document.createElement('div');
-        const wordP = document.createElement('p');
-        borderDiv.classList.add('border-div');
-
-        wordP.textContent = word;
-        // ドラッグアンドドロップを有効にする
-        borderDiv.setAttribute('draggable', 'true');
-        borderDiv.id = `hai-${this._idCounter++}`;//なくてもOK　必要になるときがあるかも？
-
-        borderDiv.addEventListener('dragstart', event => {
-            this._movedAnotherParent = false;
-            event.dataTransfer.setData('text/plain', event.target.id);
-            this._draggedElement = event.target;
-            this._originalParent = this._draggedElement.parentNode; // ドラッグ開始時の親要素を保存
-            setTimeout(() => event.target.style.opacity = '0.25', 0); // ドラッグ中は透明に
-            console.log(this._draggedElement);
-        });
-
-        borderDiv.addEventListener('dragend', event => {
-            if(!this._draggedElement)return;
-            this._draggedElement.style.opacity = '1'; // ドラッグ終了時に元に戻す
-            this._draggedElement = null;
-        });
-
-        //画像
-        borderDiv.style.backgroundImage = `url(/img/partOfSpeech/${partOfSpeech}.png)`;
-
-        borderDiv.appendChild(wordP);
-        document.getElementById('wordDown').appendChild(borderDiv);
-
-        return borderDiv;
-    }
-
-    gameStart(){
-        this.changeCondition(this._selectionDiv);
-    }
+    targetElement.addEventListener("dragover", (event) => {
+      event.preventDefault();
+    });
+  }
 }
