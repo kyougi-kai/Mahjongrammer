22f1f275485f905dd110800a1710421217bd3d03
kyougi-kai <zsansirokuma@gmail.com>
2025-04-05 12:35:22 +0900

    Update grammercheck.js

--- public/js/until/grammercheck.js
+++ public/js/until/grammercheck.js
@@ -820,6 +820,11 @@ export let tango = {
     },
 };
 
+hai = {
+    go: {},
+    theirs: {},
+};
+
 const DaimeisicanSArray = ['主格', '指示代名詞', '不定代名詞', '疑問代名詞'];
 const DaimeisicanCArray = ['主格', '所有代名詞', '再帰代名詞', '指示代名詞', '不定代名詞', '疑問代名詞'];
 const DaimeisicanOArray = ['目的格', '再帰代名詞', '指示代名詞', '不定代名詞', '疑問代名詞'];
