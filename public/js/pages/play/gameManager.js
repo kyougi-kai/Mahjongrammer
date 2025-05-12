16f33e76739712cb7d40c6520c4453626582ba82
kyougi-kai <zsansirokuma@gmail.com>
2025-05-13 08:52:10 +0900

    connect

--- public/js/pages/play/gameManager.js
+++ public/js/pages/play/gameManager.js
@@ -3,11 +3,12 @@ import { playerManager } from '/js/pages/play/playerManager.js';
 
 export class gameManager {
     constructor() {
-        this.connectionmanager = new connectionManager();
-        this.playermanager = new playerManager(null);
+        this.protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
+        const connectUrl = this.getConnectUrl();
+        this.connectionmanager = new connectionManager(connectUrl);
+        this.playermanager = new playerManager(this.connectionmanager);
     }
 
-    ここから;
     getConnectUrl() {
         return `${this.protocol}://${window.location.host}/play/${this.parentName}`;
     }
