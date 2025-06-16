8d548ae7b6f461598279dedcee48a449b19331b6
merg-hz <pb.kyougikai@gmail.com>
2025-06-16 13:24:46 +0900

    a

--- public/js/pages/play/flow.js
+++ public/js/pages/play/flow.js
@@ -147,6 +147,7 @@ export class flow {
         if (isparent) {
             this.youCanThrow = true;
             this.drawHai();
+            this.scorebords.children[4].style.opacity = 1;
         }
     }
 
