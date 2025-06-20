da808a4514113ddbc3a1808253a650c8c1299abf
merg-hz <pb.kyougikai@gmail.com>
2025-06-20 15:56:12 +0900

    Update zentisi.js

--- nagaya/zentisi.js
+++ nagaya/zentisi.js
@@ -2,170 +2,212 @@ let tango = {
     aboard: {
         tags: ['場所'], 
         hinsi: ['前置詞'],
+        means: {前置詞:'（乗りものを使って）～に乗って'},
     },
     about: { 
         tags: ['時間'], 
-        hinsi: ['前置詞'], 
+        hinsi: ['前置詞'],
+        means: {前置詞:'《関連を表して》 ～について'}, 
     },
     above: { 
         tags: ['場所', '方向'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:'～の上に'},
     },
     across: { 
         tags: ['方向', '場所'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     after: { 
         tags: ['時間'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     against: { 
         tags: ['場所'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     along: { 
         tags: ['方向'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     among: { 
         tags: ['場所'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     around: { 
         tags: ['方向', '場所'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     as: { 
         tags: ['手段'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     at: { 
         tags: ['時間', '場所'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     before: { 
         tags: ['時間'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     behind: { 
         tags: ['場所'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     beside: { 
         tags: ['場所'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     between: { 
         tags: ['場所', '時間'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     by: { 
         tags: ['手段', '場所', '時間'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     down: { 
         tags: ['方向', '場所'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     during: { 
         tags: ['時間'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     for: { 
         tags: ['時間', '手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     from: { 
         tags: ['方向', '手段'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     in: { 
         tags: ['場所', '時間'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     including: { 
         tags: ['手段'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     into: { 
         tags: ['方向'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     like: { 
         tags: ['手段'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     near: { 
         tags: ['場所'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     of: { 
         tags: ['手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     off: { 
         tags: ['方向'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     on: { 
         tags: ['場所', '時間'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     over: { 
         tags: ['方向', '場所'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     plus: { 
         tags: ['手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     since: { 
         tags: ['時間', '手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     than: { 
         tags: ['手段'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     through: { 
         tags: ['方向', '場所'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     throw: { 
         tags: ['手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     to: { 
         tags: ['方向', '手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     toward: { 
         tags: ['方向'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     under: { 
         tags: ['場所', '方向'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     until: { 
         tags: ['時間'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     up: { 
         tags: ['方向'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     upon: { 
         tags: ['方向'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
     with: { 
         tags: ['手段'], 
         hinsi: ['前置詞'],
+        means: {前置詞:''},
     },
     without: { 
         tags: ['手段'], 
         hinsi: ['前置詞'], 
+        means: {前置詞:''},
     },
 };
   
