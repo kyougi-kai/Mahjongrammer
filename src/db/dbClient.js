9164b3825e2a080696ceb35d7990cc1ced89d41e
kyougi-kai <zsansirokuma@gmail.com>
2025-10-03 10:54:21 +0900

    db修正

--- src/db/dbClient.js
+++ src/db/dbClient.js
@@ -1,6 +1,5 @@
 import mysql from 'mysql2/promise';
-import pkg from 'pg';
-const { Pool } = pkg;
+import postgres from 'postgres';
 
 export class dbClient {
     constructor() {
@@ -19,23 +18,15 @@ export class dbClient {
             });
         }
         if (process.env.env == 'production') {
-            this.pool = new Pool({
-                host: process.env.host,
-                user: process.env.user,
-                password: process.env.password,
-                database: process.env.database,
-                port: process.env.dbport || 5432,
-                ssl: { rejectUnauthorized: false }, // Supabase は SSL 必須
-                max: 20, // connectionLimit 相当
-            });
+            this.pool = postgres(process.env.DATABASE_URL);
         }
     }
 
     async query(sql, params) {
         if (process.env.env == 'production') {
-            const client = await this.pool.connect();
-            const res = await client.query(sql, params);
-            return res.rows; // MySQLのrowsに相当
+            let index = 1;
+            const sqlConverted = sql.replace(/\?/g, () => `$${index++}`);
+            return await this.pool.unsafe(sqlConverted, params);
         } else {
             const [rows] = await this.pool.execute(sql, params);
             return rows;
